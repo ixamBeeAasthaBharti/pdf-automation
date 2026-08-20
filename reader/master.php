7151b62e22ef62a075b52a63b13d64e37a3b930f
aastha <aastha.bharti@ixambee.com>
2026-08-20 14:48:49 +0530

    readded removed css

--- reader/master.php
+++ reader/master.php
@@ -110,6 +110,7 @@ $logoSrc = '../assets/logo.png';
   <link rel="preconnect" href="https://fonts.googleapis.com"/>
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
    <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,200..900;1,7..72,200..900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../styles/reader.css"/>
 </head>
 <body>
 
