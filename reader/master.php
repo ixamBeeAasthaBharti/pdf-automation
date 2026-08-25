25fa9888d5eaad42599125a54c42a188a3b41ad1
aastha <aastha.bharti@ixambee.com>
2026-08-25 16:51:26 +0530

    fin

--- reader/master.php
+++ reader/master.php
@@ -122,8 +122,11 @@ $logoSrc = '../assets/logo.png';
   <link rel="preconnect" href="https://fonts.googleapis.com"/>
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
    <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,200..900;1,7..72,200..900&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="../styles/reader.css"/>
   <link rel="stylesheet" href="../styles/pdf.css"/>
+
+
 
 </head>
 <body>
