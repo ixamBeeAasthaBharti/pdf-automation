a72947569cb0d4d71996aa82f29a320f2b5093f0
aastha <aastha.bharti@ixambee.com>
2026-08-26 10:41:02 +0530

    hindi: mangal

--- reader/master.php
+++ reader/master.php
@@ -121,7 +121,8 @@ $logoSrc = '../assets/logo.png';
   <meta name="description" content="ixamBee Study Notes - <?php echo htmlspecialchars($pageTitle, ENT_QUOTES, 'UTF-8'); ?>"/>
   <link rel="preconnect" href="https://fonts.googleapis.com"/>
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-   <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,200..900;1,7..72,200..900&display=swap" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css2?family=Literata:ital,opsz,wght@0,7..72,200..900;1,7..72,200..900&family=Noto+Sans+Devanagari:wght@400;600;700&display=swap" rel="stylesheet">
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="../styles/reader.css"/>
   <link rel="stylesheet" href="../styles/pdf.css"/>
